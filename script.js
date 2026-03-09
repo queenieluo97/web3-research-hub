66e8c63d6dc56f81f3251f3e3a1e504ded429fcd
queenieluo97 <queenie.luo97@gmail.com>
2026-03-09 14:29:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,29 @@ container.appendChild(div)
 }
 
 loadProjects()
+
+async function searchProject(){
+
+let keyword=document.getElementById("search").value
+
+let res=await fetch("projects.json")
+
+let data=await res.json()
+
+let result=data.filter(p=>p.name.toLowerCase().includes(keyword.toLowerCase()))
+
+let container=document.getElementById("projects")
+
+container.innerHTML=""
+
+result.forEach(p=>{
+
+let div=document.createElement("div")
+
+div.innerHTML=p.name+" - "+p.category
+
+container.appendChild(div)
+
+})
+
+}
